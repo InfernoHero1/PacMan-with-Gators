36d3edf78c8e5f01d5046ec2467e686d6507ecec
InfernoHero1 <32179120+InfernoHero1@users.noreply.github.com>
2017-12-02 17:54:33 -0500

    Update StudentController.java

--- src/ufl/cs1/controllers/StudentController.java
+++ src/ufl/cs1/controllers/StudentController.java
@@ -17,7 +17,7 @@ public final class StudentController implements DefenderController
 		int[] actions = new int[Game.NUM_DEFENDER];
 		List<Defender> enemies = game.getDefenders();
 		
-		//Chooses a random LEGAL action if required. Could be much simpler by simply returning
+		/*//Chooses a random LEGAL action if required. Could be much simpler by simply returning
 		//any random number of all of the ghosts
 		for(int i = 0; i < actions.length; i++)
 		{
@@ -27,7 +27,31 @@ public final class StudentController implements DefenderController
 				actions[i]=possibleDirs.get(Game.rng.nextInt(possibleDirs.size()));
 			else
 				actions[i] = -1;
-		}
+		}*/
+		actions[0] = doctorPepper(game, timeDue);
 		return actions;
 	}
+	public int doctorPepper(Game doe, long timer) {
+		int hououin = 0;
+		List<Defender> enemies = doe.getDefenders();
+		Defender defender = enemies.get(0);
+		Attacker Megan = doe.getAttacker();  //Changed name to Megan because Kassandra isn't mean to me, but Megan would be. (Drake and Josh reference)
+		//List<Node> joe = doe.getPillList();
+		//Node earl; //FIXME Need to initialize???
+
+		if (defender.isVulnerable()) {
+				//ListNode
+				Node bob = Megan.getLocation();
+				hououin = defender.getNextDir(bob, false);
+			//Get away from the yellow muncher.
+
+		}
+		else {
+			//Chase the yellow muncher.
+			Node bob = Megan.getLocation();
+			//hououin = earl.getPathDistance(bob); //FIXME GET NEXT DIRECTION WOULD BE BETTER.
+			hououin = defender.getNextDir(bob, true);
+		}
+
+		return hououin;
 }
